6777eacf26c6c2f2b644bb0ebb6f415de756867c
Abhinand <abhin4nd6@gmail.com>
2024-04-24 20:59:18 +0530

    last commit

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import {Navigate} from 'react-router-dom'
 import { UserContext, } from '../App'
 
-function ProtectedRoute() {
+function ProtectedRoute({children}) {
     const { user } = useContext(UserContext)
     if (!user) {
         return <Navigate to='/' />
